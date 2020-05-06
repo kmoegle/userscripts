de470aafb96027c1a40e2018f99f6c84c8edfa5c
Kristoffer Mögle <kmoegle@eg-factory.de>
2020-05-06 15:53:27 +0200

    Add initial draft of jira-board-custom-styles.js

---
+++ atlassian/jira/jira-board-custom-styles.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Jira Board Custom Styles
+// @namespace   info.nehrke.jira
+// @author      Kristoffer Moegle
+// @description This script will change the styles of Jira Boards to be (what I think is) more useful.
+// @include     /^https?:\/\/jira.[^\/]+\/secure\/RapidBoard.jspa.*/
+// @include     /^https?:\/\/[^\/]+?\.atlassian\.net\/secure\/RapidBoard.jspa.*/
+// @downloadURL 
+// @updateURL   
+// @version     1.0.0
+// @grant       none
+// ==/UserScript==
+
+// this script is inspired by and based on https://gist.github.com/nemoinho/70a176e73e2cbf14315772cc4e618516
+
+// the boards title is often not recognized - let's resize it to show up more obviously
+applyStyles(`
+  h1 span.subnav-page-header { font-size: 24px; font-weight: none; color: #172b4d; }
+  h1 span.subnavigator-title  { font-size: 18px; font-weight: none; color: #aaa; }
+`)
+
+function applyStyles(styles) {
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = styles;
+    document.head.appendChild(style);
+}
